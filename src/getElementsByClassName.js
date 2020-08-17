7214370571544abe4881f4f4a62d77c582332761
David <davidmcruz1125@gmail.com>
2020-08-17 16:50:07 -0500

    Create working getElementsByClassName function using recursion

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,40 +7,23 @@
 
 
 
-// get elements by class name should return an array
-// recursion starts by being called on the document.body
 var getElementsByClassName = function(className) {
-  // declare variables
 
   let result = [];
   let body = document.body;
 
-  // work on the dom
   let search = function(node) {
-    let children = node.childNodes; // if node has more nodes contained <node>
-    // this is the stop and push
-    if ( node.classList.contains(className) ) { // if <node class = 'className multipule '>
+
+    let children = node.childNodes;
+    if ( node.classList && node.classList.contains(className) ) {
       result.push(node);
     }
-    // this is the recursion
     if ( children ) {
       children.forEach( element => search(element));
     }
-    // collection.forEach starts
-    // if (stop) { result.push }
-    // if (childNodes > 0) { collection.forEach recursion }
+
   };
-
-
-
-  // [some, thing, here]
-  // classList.forEach(function( some ) { className === some return tag })
-  // if childNodes.length > 0 { recursion }
-
-
-
-
-
+ console.log('test')
   search(body);
   return result;
 };
